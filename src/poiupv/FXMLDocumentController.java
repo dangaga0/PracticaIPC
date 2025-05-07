34e58055cd6a0895950bf418892047128c8bd675
damia <damia@10.237.13.243>
2025-05-07 10:52:04 +0200

    hi?

---
+++ src/poiupv/FXMLDocumentController.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poiupv;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import poiupv.Poi;
+
+/**
+ *
+ * @author .
+ */
+public class FXMLDocumentController implements Initializable {
+
+    //=======================================
+    // hashmap para guardar los puntos de interes POI
+    private final HashMap<String, Poi> hm = new HashMap<>();
+    private ObservableList<Poi> data;
+    // ======================================
+    // la variable zoomGroup se utiliza para dar soporte al zoom
+    // el escalado se realiza sobre este nodo, al escalar el Group no mueve sus nodos
+    private Group zoomGroup;
+
+    @FXML
+    private ListView<Poi> map_listview;
+    @FXML
+    private ScrollPane map_scrollpane;
+    @FXML
+    private Slider zoom_slider;
+    
+    private MenuItem pin_info;
+    @FXML
+    private SplitPane splitPane;
+    @FXML
+    private Label mousePosition;
+    @FXML
+    private ToggleGroup paintingOptions;
+
+    void zoomIn(ActionEvent event) {
+        //================================================
+        // el incremento del zoom dependerá de los parametros del 
+        // slider y del resultado esperado
+        double sliderVal = zoom_slider.getValue();
+        zoom_slider.setValue(sliderVal += 0.1);
+    }
+
+    void zoomOut(ActionEvent event) {
+        double sliderVal = zoom_slider.getValue();
+        zoom_slider.setValue(sliderVal + -0.1);
+    }
+    
+    // esta funcion es invocada al cambiar el value del slider zoom_slider
+    private void zoom(double scaleValue) {
+        //===================================================
+        //guardamos los valores del scroll antes del escalado
+        double scrollH = map_scrollpane.getHvalue();
+        double scrollV = map_scrollpane.getVvalue();
+        //===================================================
+        // escalamos el zoomGroup en X e Y con el valor de entrada
+        zoomGroup.setScaleX(scaleValue);
+        zoomGroup.setScaleY(scaleValue);
+        //===================================================
+        // recuperamos el valor del scroll antes del escalado
+        map_scrollpane.setHvalue(scrollH);
+        map_scrollpane.setVvalue(scrollV);
+    }
+
+    @FXML
+    void listClicked(MouseEvent event) {
+        
+    }
+
+    private void initData() {
+        data=map_listview.getItems();
+        data.add(new Poi("1F", "Edificion del DSIC", 275, 250));
+        data.add( new Poi("Agora", "Agora", 575, 350));
+        data.add( new Poi("Pista", "Pista de atletismo y campo de futbol", 950, 350));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Code from ChatGPT to avoid getting the focuss on any button when open
+        Platform.runLater(() -> {
+           map_scrollpane.requestFocus();
+        });
+        //==========================================================
+        // inicializamos el slider y enlazamos con el zoom
+        zoom_slider.setMin(0.2);
+        zoom_slider.setMax(1.5);
+        zoom_slider.setValue(1.0);
+        zoom_slider.valueProperty().addListener((o, oldVal, newVal) -> zoom((Double) newVal));
+
+        //=========================================================================
+        //Envuelva el contenido de scrollpane en un grupo para que 
+        //ScrollPane vuelva a calcular las barras de desplazamiento tras el escalado
+        Group contentGroup = new Group();
+        zoomGroup = new Group();
+        contentGroup.getChildren().add(zoomGroup);
+        zoomGroup.getChildren().add(map_scrollpane.getContent());
+        map_scrollpane.setContent(contentGroup);
+
+    }
+
+    @FXML
+    private void showPosition(MouseEvent event) {
+        mousePosition.setText("sceneX: " + (int) event.getSceneX() + ", sceneY: " + (int) event.getSceneY() + "\n"
+                + "         X: " + (int) event.getX() + ",          Y: " + (int) event.getY());
+    }
+
+    private void closeApp(ActionEvent event) {
+        ((Stage) zoom_slider.getScene().getWindow()).close();
+    }
+
+    @FXML
+    private void about(ActionEvent event) {
+        Alert mensaje = new Alert(Alert.AlertType.INFORMATION);
+        // Acceder al Stage del Dialog y cambiar el icono
+        Stage dialogStage = (Stage) mensaje.getDialogPane().getScene().getWindow();
+        dialogStage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/logo.png")));
+        mensaje.setTitle("Acerca de");
+        mensaje.setHeaderText("IPC - 2025");
+        mensaje.showAndWait();
+    }
+
+    @FXML
+    private void zoomScroll(ScrollEvent event) {
+        
+        //apply the scroll to the zoom
+        double sliderVal = zoom_slider.getValue();
+        zoom_slider.setValue(sliderVal + (event.getDeltaY() * 0.001));
+        
+        //avoid moving the map
+         event.consume();
+    }
+}
